6bf73df27c108254bb0c52addbc76ecd3bfa5461
Maly <migsking@naver.com>
2021-06-03 12:53:19 +0900

    .

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class User:
-    name = os.getenv("USER_NAME"),
-    birth = os.getenv("USER_BIRTH"),
-    password = os.getenv("PASSWORD"),
+    name = os.getenv("USER_NAME")
+    birth = os.getenv("USER_BIRTH")
+    password = os.getenv("PASSWORD")
     org_code = os.getenv("ORG_CODE")
--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from config import User
 from app import AutoSelfTest
 
 app = AutoSelfTest(User.name, User.birth, User.org_code, User.password)
+print(User.name)
 app.run()
